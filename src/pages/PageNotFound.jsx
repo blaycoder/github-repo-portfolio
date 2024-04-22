9d31d6b103b60644e93eed1754296f621cdb8e12
blaycoder <onatola4real@gmail.com>
2024-04-22 22:52:15 +0100

    removing unneccesary comments

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -4,14 +4,6 @@ import { Link } from "react-router-dom";
 export default function PageNotFound() {
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
@@ -30,9 +22,6 @@ export default function PageNotFound() {
                 Go back home
               </button>
             </Link>
-            {/* <a href="#" className="text-sm font-semibold text-gray-900">
-              Contact support <span aria-hidden="true">&rarr;</span>
-            </a> */}
           </div>
         </div>
       </main>
