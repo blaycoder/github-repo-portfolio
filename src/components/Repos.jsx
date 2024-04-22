6690ec2526bf3027eb7fc121076258e7c88d1105
blaycoder <onatola4real@gmail.com>
2024-04-22 23:11:15 +0100

    made changes to repos file

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -5,10 +5,6 @@ import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
 import { SearchField } from "@adobe/react-spectrum";
 
-// import Search from "./Search";
-// import Pagination from "./Pagination";
-// import {Link} from "react-aria-components"
-
 function Repos() {
   const { isLoading, error, data } = useQuery("repos", useFetchRepos);
   const [query, setQuery] = useState("");
@@ -28,6 +24,7 @@ function Repos() {
       </div>
     );
   if (error) return <div>Error: {error.message}</div>;
+
   return (
     <section>
       <div className="flex justify-center mt-5">
@@ -38,36 +35,32 @@ function Repos() {
         />
       </div>
 
-     {filteredRepos.length === 0 ?( 
-       <span className="text-center">No repository of such name</span>
-     ):
-     (
-       <div>
+      {filteredRepos.length === 0 ? (
+        <span className="text-center">No repository of such name</span>
+      ) : (
         <ul
           role="list"
-          className=" divide-y divide-gray-100 flex flex-col justify-center content-center"
-          >
-          {data.map((repo) => (
-            // eslint-disable-next-line react/jsx-key
+          className="divide-y divide-gray-100 flex flex-col justify-center content-center"
+        >
+          {filteredRepos.map((repo) => (
             <li
-            key={repo.id}
-            className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-5 m-5 rounded-xl"
+              key={repo.id}
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-5 m-5 rounded-xl"
             >
               <Link
                 to={`/repos/${repo.name}`}
                 className="text-white text-xl self-center flex justify-between py-5 hover:underline hover:text-white"
-                >
+              >
                 {repo.name}
               </Link>
               <div className="line-clamp-1 text-slate-300">
-                {repo.description || "No description"}{" "}
+                {repo.description || "No description"}
               </div>
             </li>
           ))}
         </ul>
-        <Outlet />
-      </div>
-        )} 
+      )}
+      <Outlet />
     </section>
   );
 }
